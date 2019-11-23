aa093f106c3bc79d49f2dbf17df810579e4af633
Rohit Yadav <rohit.yadav@shapeblue.com>
2019-11-23 21:57:10 +0530

    details autocompletion added
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- src/components/view/DetailSettings.vue
+++ src/components/view/DetailSettings.vue
@@ -16,13 +16,23 @@
 // under the License.
 
 <template>
-  <div>
+  <a-spin :spinning="loading">
     <div v-show="!showAddDetail">
       <a-button type="dashed" style="width: 100%" icon="plus" @click="showAddDetail = true">Add Setting</a-button>
     </div>
     <div v-show="showAddDetail">
-      <a-input placeholder="Name"/>
-      <a-input placeholder="Value"/>
+      <a-auto-complete
+        style="width: 100%"
+        :value="newKey"
+        :dataSource="Object.keys(detailOptions)"
+        placeholder="Name"
+        @change="e => onAddInputChange(e, 'newKey')" />
+      <a-auto-complete
+        style="width: 100%"
+        :value="newValue"
+        :dataSource="detailOptions[newKey]"
+        placeholder="Value"
+        @change="e => onAddInputChange(e, 'newValue')" />
       <a-button type="dashed" style="width: 50%" icon="close" @click="showAddDetail = false">Cancel</a-button>
       <a-button type="primary" style="width: 50%" icon="plus" @click="addDetail">Add Setting</a-button>
     </div>
@@ -32,8 +42,13 @@
           <span slot="title">{{ item.name }}</span>
           <span slot="description" style="word-break: break-all">
             <span v-if="item.edit" style="display: flex">
-              <a-input :value="item.value" @change="e => handleInputChange(e, index)" />
-              <a-button shape="circle" size="small" @click="updateDetail(item)" style="margin: 2px">
+              <a-auto-complete
+                style="width: 100%"
+                :value="item.value"
+                :dataSource="detailOptions[item.name]"
+                @change="val => handleInputChange(val, index)"
+                @pressEnter="e => updateDetail(index)" />
+              <a-button shape="circle" size="small" @click="updateDetail(index)" style="margin: 2px">
                 <a-icon type="check-circle" theme="twoTone" twoToneColor="#52c41a" style="font-size: 24px"/>
               </a-button>
               <a-button shape="circle" size="small" @click="hideEditDetail(index)" style="margin: 2px">
@@ -51,7 +66,7 @@
         <div slot="actions">
           <a-popconfirm
             title="Delete setting?"
-            @confirm="deleteDetail(item)"
+            @confirm="deleteDetail(index)"
             okText="Yes"
             cancelText="No"
             placement="left"
@@ -63,37 +78,56 @@
         </div>
       </a-list-item>
     </a-list>
-  </div>
+  </a-spin>
 </template>
 
 <script>
+import { api } from '@/api'
+
 export default {
   name: 'DetailSettings',
   props: {
     resource: {
       type: Object,
       required: true
-    },
-    loading: {
-      type: Boolean,
-      default: false
     }
   },
   data () {
     return {
       details: [],
-      showAddDetail: false
+      detailOptions: {},
+      showAddDetail: false,
+      newKey: '',
+      newValue: '',
+      loading: false,
+      resourceType: 'UserVm'
+    }
+  },
+  watch: {
+    resource: function (newItem, oldItem) {
+      this.updateResource(newItem)
     }
   },
   mounted () {
-    this.details = Object.keys(this.resource.details).map(k => {
-      return { name: k, value: this.resource.details[k] }
-    })
-    this.details.forEach(function (detail, index) {
-      detail.edit = false
-    })
+    this.updateResource(this.resource)
   },
   methods: {
+    updateResource (resource) {
+      if (!resource) {
+        return
+      }
+      this.resource = resource
+      this.resourceType = this.$route.meta.resourceType
+      if (!resource.details) {
+        return
+      }
+      this.details = Object.keys(this.resource.details).map(k => {
+        return { name: k, value: this.resource.details[k], edit: false }
+      })
+      api('listDetailOptions', { resourcetype: this.resourceType, resourceid: this.resource.id }).then(json => {
+        this.detailOptions = json.listdetailoptionsresponse.detailoptions.details
+      })
+    },
     showEditDetail (index) {
       this.details[index].edit = true
       this.details[index].originalValue = this.details[index].value
@@ -104,22 +138,66 @@ export default {
       this.details[index].value = this.details[index].originalValue
       this.$set(this.details, index, this.details[index])
     },
-    handleInputChange (e, index) {
-      this.details[index].value = e.target.value
+    handleInputChange (val, index) {
+      this.details[index].value = val
       this.$set(this.details, index, this.details[index])
     },
+    onAddInputChange (val, obj) {
+      this[obj] = val
+    },
+    runApi () {
+      var apiName = ''
+      if (this.resourceType === 'UserVm') {
+        apiName = 'updateVirtualMachine'
+      } else if (this.resourceType === 'Template') {
+        apiName = 'updateTemplate'
+      }
+      if (!(apiName in this.$store.getters.apis)) {
+        this.$notification.error({
+          message: 'Failed to execute API: ' + apiName,
+          description: 'User is not permitted to use the API'
+        })
+        return
+      }
+
+      const params = { id: this.resource.id }
+      this.details.forEach(function (item, index) {
+        params['details[0].' + item.name] = item.value
+      })
+      this.loading = true
+      api(apiName, params).then(json => {
+        var details = {}
+        if (this.resourceType === 'UserVm') {
+          details = json.updatevirtualmachineresponse.virtualmachine.details
+        } else if (this.resourceType === 'Template') {
+          details = json.updatetemplateresponse.template.details
+        }
+        this.details = Object.keys(details).map(k => {
+          return { name: k, value: details[k], edit: false }
+        })
+      }).catch(error => {
+        this.$notification.error({
+          message: 'Failed to add setting',
+          description: error.response.headers['x-description']
+        })
+      }).finally(f => {
+        this.loading = false
+        this.showAddDetail = false
+        this.newKey = ''
+        this.newValue = ''
+      })
+    },
     addDetail () {
-      console.log('adding detail')
+      this.details.push({ name: this.newKey, value: this.newValue })
+      this.runApi()
     },
-    updateDetail (item) {
-      console.log(item)
+    updateDetail (index) {
+      this.runApi()
     },
-    deleteDetail (item) {
-      console.log(item)
+    deleteDetail (index) {
+      this.details.splice(index, 1)
+      this.runApi()
     }
   }
 }
 </script>
-
-<style scoped>
-</style>
