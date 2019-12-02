4aa62e1d9077d504142df771ef68698dc065c32c
Ritchie Vincent <ritchie@ritchievincent.co.uk>
2019-12-02 10:37:27 +0000

    Add POST axios method

--- src/api/index.js
+++ src/api/index.js
@@ -25,6 +25,17 @@ export function api (command, args = {}) {
   })
 }
 
+export function apiPostForm (command, data = {}) {
+  return axios({
+    params: {
+      command,
+      response: 'json'
+    },
+    method: 'POST',
+    data
+  })
+}
+
 export function login (arg) {
   const params = new URLSearchParams()
   params.append('command', 'login')
