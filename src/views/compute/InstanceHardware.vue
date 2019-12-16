0233b68b12e4f9e7e2d3e6e2294fcca7659bb003
Ritchie Vincent <ritchie@ritchievincent.co.uk>
2019-12-16 02:06:32 +0000

    Add VM NICs and IPs management

--- src/views/compute/InstanceHardware.vue
+++ src/views/compute/InstanceHardware.vue
@@ -64,12 +64,17 @@
 
       </a-collapse-panel>
       <a-collapse-panel :header="'Network Adapter(s): ' + (vm && vm.nic ? vm.nic.length : 0)" key="3" >
+        <a-button type="primary" style="display: block; margin-left: auto;" @click="showAddModal" :loading="loadingNIC">
+          <a-icon type="plus-circle"></a-icon> Add network to VM
+        </a-button>
         <a-list
           size="small"
           itemLayout="horizontal"
           :dataSource="vm.nic"
+          class="list"
+          :loading="loadingNIC"
         >
-          <a-list-item slot="renderItem" slot-scope="item">
+          <a-list-item slot="renderItem" slot-scope="item" class="list__item">
             <a-list-item-meta>
               <div slot="title">
                 <span v-show="item.isdefault">(Default) </span>
@@ -91,10 +96,140 @@
               Broadcast URI: {{ item.broadcasturi }}<br/>
               Isolation URI: {{ item.isolationuri }}<br/>
             </p>
+            <div slot="actions" class="actions">
+              <a-tooltip placement="left">
+                <template slot="title">
+                  {{ "Edit IP Address" }}
+                </template>
+                <a-button
+                  type="primary"
+                  icon="edit"
+                  shape="circle"
+                  @click="editIpAddressNIC = item.id; showUpdateIPModal = true" />
+              </a-tooltip>
+              <a-tooltip placement="left">
+                <template slot="title">
+                  {{ "Edit/Add secondary IP Address" }}
+                </template>
+                <a-button icon="edit" shape="circle" @click="fetchSecondaryIPs(item)" />
+              </a-tooltip>
+              <a-tooltip placement="left" v-if="!item.isdefault">
+                <template slot="title">
+                  {{ "Set as default" }}
+                </template>
+                <a-popconfirm
+                  title="Confirm set as default?"
+                  @confirm="setAsDefault(item)"
+                  okText="Yes"
+                  cancelText="No"
+                >
+                  <a-button icon="form" shape="circle" />
+                </a-popconfirm>
+              </a-tooltip>
+              <a-tooltip placement="left" v-if="!item.isdefault">
+                <template slot="title">
+                  {{ "Remove" }}
+                </template>
+                <a-popconfirm
+                  :title="$t('message.network.removeNIC')"
+                  @confirm="removeNIC(item)"
+                  okText="Yes"
+                  cancelText="No"
+                >
+                  <a-button type="danger" icon="delete" shape="circle" />
+                </a-popconfirm>
+              </a-tooltip>
+            </div>
           </a-list-item>
         </a-list>
       </a-collapse-panel>
     </a-collapse>
+
+    <a-modal
+      :visible="showAddNetworkModal"
+      :title="$t('label.network.addVM')"
+      @cancel="closeModals"
+      @ok="submitAddNetwork">
+      {{ $t('message.network.addVM.desc') }}
+
+      <div class="modal-form">
+        <p class="modal-form__label">{{ $t('Network') }}:</p>
+        <a-select :defaultValue="addNetworkData.network" @change="e => addNetworkData.network === e">
+          <a-select-option
+            v-for="network in addNetworkData.allNetworks"
+            :key="network.id"
+            :value="network.id">
+            {{ network.name }}
+          </a-select-option>
+        </a-select>
+        <p class="modal-form__label">{{ $t('publicip') }}:</p>
+        <a-input v-model="addNetworkData.ip"></a-input>
+      </div>
+
+    </a-modal>
+
+    <a-modal
+      :visible="showUpdateIPModal"
+      :title="$t('label.change.ipaddress')"
+      @cancel="closeModals"
+      @ok="submitUpdateIP"
+    >
+      {{ $t('message.network.updateIp') }}
+
+      <div class="modal-form">
+        <p class="modal-form__label">{{ $t('publicip') }}:</p>
+        <a-input v-model="editIPAddressValue"></a-input>
+      </div>
+
+    </a-modal>
+
+    <a-modal
+      :visible="showSecondaryIPModal"
+      :title="$t('label.acquire.new.secondary.ip')"
+      :footer="null"
+      class="wide-modal"
+    >
+      <p>
+        {{ $t('message.network.secondaryIP') }}
+      </p>
+      <a-divider />
+      <p class="modal-form__label">Add new secondary {{ $t('publicip') }}:</p>
+      <a-input placeholder="Enter new secondary IP Address" v-model="newSecondaryIP"></a-input>
+
+      <div style="margin-top: 10px; display: flex; justify-content:flex-end;">
+        <a-button @click="submitSecondaryIP" type="primary" style="margin-right: 10px;">Add</a-button>
+        <a-button @click="closeModals">Cancel</a-button>
+      </div>
+
+      <a-divider />
+      <a-list itemLayout="vertical">
+        <a-list-item v-for="(ip, index) in secondaryIPs" :key="index">
+          <p class="modal-form__label modal-form__label--no-margin">IP Address:</p>
+          {{ ip.ipaddress }}
+          <p class="modal-form__label modal-form__label--no-margin">ID:</p>
+          {{ ip.id }}
+          <p class="modal-form__label modal-form__label--no-margin">VM Name:</p>
+          {{ vm.name }}
+          <p class="modal-form__label modal-form__label--no-margin">Zone Name:</p>
+          {{ vm.zonename }}
+
+          <a-popconfirm
+            title="Release IP?"
+            @confirm="removeSecondaryIP(ip.id)"
+            okText="Yes"
+            cancelText="No"
+          >
+            <a-button
+              style="display: block; margin-top: 10px; margin-bottom: -10px;"
+              type="danger"
+              shape="circle"
+              icon="delete" />
+          </a-popconfirm>
+
+        </a-list-item>
+      </a-list>
+    </a-modal>
+
   </div>
 </template>
 
@@ -120,12 +255,27 @@ export default {
       default: false
     }
   },
+  inject: ['parentFetchData'],
   data () {
     return {
       vm: {},
       volumes: [],
       totalStorage: 0,
-      activeKey: ['1', '2', '3']
+      activeKey: ['1', '2', '3'],
+      showAddNetworkModal: false,
+      showUpdateIPModal: false,
+      showSecondaryIPModal: false,
+      addNetworkData: {
+        allNetworks: [],
+        network: '',
+        ip: ''
+      },
+      loadingNIC: false,
+      editIpAddressNIC: '',
+      editIPAddressValue: '',
+      secondaryIPs: [],
+      selectedSecondaryIPNIC: '',
+      newSecondaryIP: ''
     }
   },
   created () {
@@ -154,12 +304,274 @@ export default {
         }
         this.$set(this.resource, 'volumes', this.volumes)
       })
+    },
+    listNetworks () {
+      api('listNetworks', {
+        response: 'json',
+        listAll: 'true',
+        zoneid: this.vm.zoneid
+      }).then(response => {
+        this.addNetworkData.allNetworks = response.listnetworksresponse.network
+        this.addNetworkData.network = this.addNetworkData.allNetworks[0].id
+      })
+    },
+    fetchSecondaryIPs (item) {
+      this.showSecondaryIPModal = true
+      this.selectedSecondaryIPNIC = item.id
+      api('listNics', {
+        response: 'json',
+        nicId: item.id,
+        keyword: '',
+        virtualmachineid: this.vm.id
+      }).then(response => {
+        this.secondaryIPs = response.listnicsresponse.nic[0].secondaryip
+      })
+    },
+    showAddModal () {
+      this.showAddNetworkModal = true
+      this.listNetworks()
+    },
+    closeModals () {
+      this.showAddNetworkModal = false
+      this.showUpdateIPModal = false
+      this.showSecondaryIPModal = false
+      this.addNetworkData.network = ''
+      this.addNetworkData.ip = ''
+
+      this.editIPAddressValue = ''
+
+      this.newSecondaryIP = ''
+    },
+    submitAddNetwork () {
+      this.loadingNIC = true
+      this.showAddNetworkModal = false
+      api('addNicToVirtualMachine', {
+        response: 'json',
+        virtualmachineid: this.vm.id,
+        networkid: this.addNetworkData.network,
+        ipaddress: this.addNetworkData.ip
+      }).then(response => {
+        this.queryAsyncJobResult({
+          jobId: response.addnictovirtualmachineresponse.jobid,
+          successMessage: `Successfully added network`,
+          successMethod: () => {
+            this.loadingNIC = false
+            this.closeModals()
+            this.parentFetchData()
+          },
+          errorMessage: 'Adding network failed',
+          errorMethod: () => {
+            this.loadingNIC = false
+            this.closeModals()
+            this.parentFetchData()
+          },
+          loadingMessage: `Adding network...`,
+          catchMessage: 'Error encountered while fetching async job result',
+          catchMethod: () => {
+            this.loadingNIC = false
+            this.closeModals()
+            this.parentFetchData()
+          }
+        })
+      }).catch(error => {
+        this.$notification.error({
+          message: `Error ${error.response.status}`,
+          description: error.response.data.errorresponse.errortext
+        })
+        this.loadingNIC = false
+      })
+    },
+    setAsDefault (item) {
+      this.loadingNIC = true
+      api('updateDefaultNicForVirtualMachine', {
+        response: 'json',
+        virtualmachineid: this.vm.id,
+        nicid: item.id
+      }).then(response => {
+        this.queryAsyncJobResult({
+          jobId: response.updatedefaultnicforvirtualmachineresponse.jobid,
+          successMessage: `Successfully set ${item.networkname} to default. Please manually update the default NIC on the VM now.`,
+          successMethod: () => {
+            this.loadingNIC = false
+            this.parentFetchData()
+          },
+          errorMessage: `Error setting ${item.networkname} to default`,
+          errorMethod: () => {
+            this.loadingNIC = false
+            this.parentFetchData()
+          },
+          loadingMessage: `Setting ${item.networkname} to default...`,
+          catchMessage: 'Error encountered while fetching async job result',
+          catchMethod: () => {
+            this.loadingNIC = false
+            this.parentFetchData()
+          }
+        })
+      }).catch(error => {
+        this.$notification.error({
+          message: `Error ${error.response.status}`,
+          description: error.response.data.errorresponse.errortext
+        })
+        this.loadingNIC = false
+      })
+    },
+    submitUpdateIP () {
+      this.loadingNIC = true
+      this.showUpdateIPModal = false
+      api('updateVmNicIp', {
+        response: 'json',
+        nicId: this.editIpAddressNIC,
+        ipaddress: this.editIPAddressValue
+      }).then(response => {
+        this.queryAsyncJobResult({
+          jobId: response.updatevmnicipresponse.jobid,
+          successMessage: `Successfully updated IP Address`,
+          successMethod: () => {
+            this.loadingNIC = false
+            this.closeModals()
+            this.parentFetchData()
+          },
+          errorMessage: `Error`,
+          errorMethod: () => {
+            this.loadingNIC = false
+            this.closeModals()
+            this.parentFetchData()
+          },
+          loadingMessage: `Updating IP Address...`,
+          catchMessage: 'Error encountered while fetching async job result',
+          catchMethod: () => {
+            this.loadingNIC = false
+            this.closeModals()
+            this.parentFetchData()
+          }
+        })
+      })
+        .catch(error => {
+          this.$notification.error({
+            message: `Error ${error.response.status}`,
+            description: error.response.data.errorresponse.errortext
+          })
+          this.loadingNIC = false
+        })
+    },
+    removeNIC (item) {
+      this.loadingNIC = true
+
+      api('removeNicFromVirtualMachine', {
+        response: 'json',
+        nicid: item.id,
+        virtualmachineid: this.vm.id
+      }).then(response => {
+        this.queryAsyncJobResult({
+          jobId: response.removenicfromvirtualmachineresponse.jobid,
+          successMessage: `Successfully removed`,
+          successMethod: () => {
+            this.loadingNIC = false
+            this.parentFetchData()
+          },
+          errorMessage: `There was an error`,
+          errorMethod: () => {
+            this.loadingNIC = false
+            this.parentFetchData()
+          },
+          loadingMessage: `Removing NIC...`,
+          catchMessage: 'Error encountered while fetching async job result',
+          catchMethod: () => {
+            this.loadingNIC = false
+            this.parentFetchData()
+          }
+        })
+      })
+        .catch(error => {
+          this.$notification.error({
+            message: `Error ${error.response.status}`,
+            description: error.response.data.errorresponse.errortext
+          })
+          this.loadingNIC = false
+        })
+    },
+    submitSecondaryIP () {
+      this.loadingNIC = true
+
+      api('addIpToNic', {
+        response: 'json',
+        nicid: this.selectedSecondaryIPNIC,
+        ipaddress: this.newSecondaryIP
+      }).then(response => {
+        this.queryAsyncJobResult({
+          jobId: response.addiptovmnicresponse.jobid,
+          successMessage: `Successfully added secondary IP Address`,
+          successMethod: () => {
+            this.loadingNIC = false
+            this.closeModals()
+            this.parentFetchData()
+          },
+          errorMessage: `There was an error adding the secondary IP Address`,
+          errorMethod: () => {
+            this.loadingNIC = false
+            this.closeModals()
+            this.parentFetchData()
+          },
+          loadingMessage: `Add Secondary IP address...`,
+          catchMessage: 'Error encountered while fetching async job result',
+          catchMethod: () => {
+            this.loadingNIC = false
+            this.closeModals()
+            this.parentFetchData()
+          }
+        })
+      })
+        .catch(error => {
+          this.$notification.error({
+            message: `Error ${error.response.status}`,
+            description: error.response.data.addiptovmnicresponse.errortext
+          })
+          this.loadingNIC = false
+        })
+    },
+    removeSecondaryIP (id) {
+      this.loadingNIC = true
+
+      api('removeIpFromNic', {
+        response: 'json',
+        id
+      }).then(response => {
+        this.queryAsyncJobResult({
+          jobId: response.removeipfromnicresponse.jobid,
+          successMessage: `Successfully removed secondary IP Address`,
+          successMethod: () => {
+            this.loadingNIC = false
+            this.closeModals()
+            this.parentFetchData()
+          },
+          errorMessage: `There was an error removing the secondary IP Address`,
+          errorMethod: () => {
+            this.loadingNIC = false
+            this.closeModals()
+            this.parentFetchData()
+          },
+          loadingMessage: `Removing Secondary IP address...`,
+          catchMessage: 'Error encountered while fetching async job result',
+          catchMethod: () => {
+            this.loadingNIC = false
+            this.closeModals()
+            this.parentFetchData()
+          }
+        })
+      })
+        .catch(error => {
+          this.$notification.error({
+            message: `Error ${error.response.status}`,
+            description: error.response.data.errorresponse.errortext
+          })
+          this.loadingNIC = false
+        })
     }
   }
 }
 </script>
 
-<style lang="less" scoped>
+<style lang="scss" scoped>
 .page-header-wrapper-grid-content-main {
   width: 100%;
   height: 100%;
@@ -176,4 +588,73 @@ export default {
     margin-bottom: 8px;
   }
 }
+
+  .list {
+    margin-top: 20px;
+
+    &__item {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+
+      @media (min-width: 760px) {
+        flex-direction: row;
+        align-items: center;
+      }
+
+    }
+
+  }
+
+  .modal-form {
+    display: flex;
+    flex-direction: column;
+
+    &__label {
+      margin-top: 20px;
+      margin-bottom: 5px;
+      font-weight: bold;
+
+      &--no-margin {
+        margin-top: 0;
+      }
+
+    }
+
+  }
+
+  .actions {
+    display: flex;
+    margin-left: -24px;
+
+    button {
+
+      &:not(:last-child) {
+        margin-right: 10px;
+      }
+
+    }
+
+    @media (min-width: 760px) {
+      flex-direction: column;
+      margin-left: 24px;
+
+      button {
+
+        &:not(:last-child) {
+          margin-bottom: 10px;
+          margin-right: 0;
+        }
+
+      }
+
+    }
+
+  }
+</style>
+
+<style lang="scss">
+  .wide-modal {
+    min-width: 60vw;
+  }
 </style>
