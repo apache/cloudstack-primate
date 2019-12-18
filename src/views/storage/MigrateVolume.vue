690cc90869c31c48f8deb1272e6e174d3e47ebc4
Rohit Yadav <rohit.yadav@shapeblue.com>
2019-12-18 16:12:27 +0530

    licence and minor nits
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- src/views/storage/MigrateVolume.vue
+++ src/views/storage/MigrateVolume.vue
@@ -1,3 +1,20 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 <template>
   <div class="migrate-volume-container">
 
@@ -30,10 +47,10 @@
 
     <div class="actions">
       <a-button @click="closeModal">
-        Cancel
+       {{ $t('Cancel') }}
       </a-button>
       <a-button type="primary" @click="submitMigrateVolume">
-        Ok
+       {{ $t('OK') }}
       </a-button>
     </div>
 
@@ -143,9 +160,8 @@ export default {
     max-width: 100%;
 
     @media (min-width: 760px) {
-      width: 65vw;
+      width: 50vw;
     }
-
   }
 
   .actions {
@@ -154,13 +170,10 @@ export default {
     margin-top: 20px;
 
     button {
-
       &:not(:last-child) {
         margin-right: 10px;
       }
-
     }
-
   }
 
   .modal-form {
