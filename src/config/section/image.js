5556ac3930332132b6acd7366583bc2d668a6f3e
Pearl Dsilva <pearl.dsilva@shapeblue.com>
2020-01-06 16:08:47 +0530

    Added checks for disabling the icon for updating template permission

--- src/config/section/image.js
+++ src/config/section/image.js
@@ -90,7 +90,7 @@ export default {
           label: 'Update template permissions',
           dataView: true,
           popup: true,
-          show: (record, user) => { return (['Admin', 'DomainAdmin'].includes(user.roletype) && (record.domainid === user.domainid && record.account === user.account) || record.templatetype !== 'SYSTEM') },
+          show: (record, user) => { return (['Admin', 'DomainAdmin'].includes(user.roletype) && (record.domainid === user.domainid && record.account === user.account) || record.templatetype !== 'BUILTIN') },
           component: () => import('@/views/image/UpdateTemplatePermissions')
         },
         {
