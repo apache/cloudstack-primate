aa9b7168c7b4542fd933d2eaa88cfb857bf87d00
Pearl Dsilva <pearl.dsilva@shapeblue.com>
2020-01-06 14:45:51 +0530

    added License

--- src/views/image/UpdateTemplatePermissions.vue
+++ src/views/image/UpdateTemplatePermissions.vue
@@ -1,3 +1,19 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
 <template>
   <div class="form">
     <div v-if="loading" class="loading">
@@ -97,7 +113,8 @@ export default {
       selectedAccounts: [],
       permittedProjects: [],
       selectedProjects: [],
-      loading: false
+      loading: false,
+      roletype: this.$store.getters.userInfo.roletype
     }
   },
   computed: {
@@ -123,6 +140,7 @@ export default {
   },
   methods: {
     fetchData () {
+      console.log('admin? = ', this.$store.getters.userInfo.roletype)
       this.fetchTemplatePermissions()
       if (this.selectedShareWith === 'Account') {
         this.selectedAccounts = []
