a30ee93d744f9aac4274280391f2e126a0b64874
Pearl Dsilva <pearl.dsilva@shapeblue.com>
2020-01-21 22:03:39 +0530

    Add secondary storage form

--- src/config/section/infra/secondaryStorages.js
+++ src/config/section/infra/secondaryStorages.js
@@ -35,7 +35,9 @@ export default {
       icon: 'plus',
       label: 'label.add.secondary.storage',
       listView: true,
-      args: ['name', 'provider', 'zoneid', 'url', 'details']
+      // args: ['name', 'provider', 'zoneid', 'url', 'details'],
+      popup: true,
+      component: () => import('@/views/infra/AddSecondaryStorage.vue')
     },
     {
       api: 'deleteImageStore',
