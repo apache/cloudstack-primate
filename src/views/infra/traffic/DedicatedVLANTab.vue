a180c0db34e27ff8f73b89c754f2443531e17edc
Ritchie Vincent <ritchie@ritchievincent.co.uk>
2020-01-23 18:57:34 +0000

    Add dedicated VLAN/VNI Ranges functionality

--- src/views/infra/traffic/DedicatedVLANTab.vue
+++ src/views/infra/traffic/DedicatedVLANTab.vue
@@ -17,11 +17,114 @@
 
 <template>
   <a-spin :spinning="fetchLoading">
-    listDedicatedGuestVlanRanges here
+    <a-button type="primary" @click="handleOpenModal">{{ $t('label.dedicate.vlan.vni.range') }}</a-button>
+    <a-divider></a-divider>
+    <a-list class="list">
+      <a-list-item v-for="item in items" :key="item.id" class="list__item">
+        <div class="list__item-outer-container">
+          <div class="list__item-container">
+            <div class="list__col">
+              <div class="list__label">{{ $t('vlanrange') }}</div>
+              <div>{{ item.guestvlanrange }}</div>
+            </div>
+            <div class="list__col">
+              <div class="list__label">{{ $t('domain') }}</div>
+              <div>{{ item.domain }}</div>
+            </div>
+            <div class="list__col">
+              <div class="list__label">{{ $t('account') }}</div>
+              <div>{{ item.account }}</div>
+            </div>
+          </div>
+          <div class="list__col">
+            <div class="list__label">{{ $t('id') }}</div>
+            <div>{{ item.id }}</div>
+          </div>
+        </div>
+        <div slot="actions">
+          <a-popconfirm
+            :title="`${$t('label.delete')}?`"
+            @confirm="handleDelete(item)"
+            okText="Yes"
+            cancelText="No"
+            placement="top"
+          >
+            <a-button icon="delete" type="danger" shape="round"></a-button>
+          </a-popconfirm>
+        </div>
+      </a-list-item>
+    </a-list>
+
+    <a-modal v-model="modal" :title="$t('label.dedicate.vlan.vni.range')" @ok="handleSubmit">
+      <a-spin :spinning="formLoading">
+        <a-form
+          :form="form"
+          @submit="handleSubmit"
+          layout="vertical" >
+          <a-form-item :label="$t('vlanRange')">
+            <a-input
+              v-decorator="['range', {
+                rules: [{ required: true, message: 'Required' }]
+              }]"
+            ></a-input>
+          </a-form-item>
+
+          <a-form-item :label="$t('scope')">
+            <a-select defaultValue="account" v-model="selectedScope" @change="handleScopeChange">
+              <a-select-option value="account">{{ $t('account') }}</a-select-option>
+              <a-select-option value="project">{{ $t('project') }}</a-select-option>
+            </a-select>
+          </a-form-item>
+
+          <a-form-item :label="$t('domain')">
+            <a-select
+              @change="handleDomainChange"
+              v-decorator="['domain', {
+                rules: [{ required: true, message: 'Required' }]
+              }]"
+            >
+              <a-select-option v-for="domain in domains" :key="domain.id" :value="domain.id">{{ domain.name }}</a-select-option>
+            </a-select>
+          </a-form-item>
+
+          <a-form-item :label="$t('account')" v-if="selectedScope === 'account'">
+            <a-select
+              v-decorator="['account', {
+                rules: [{ required: true, message: 'Required' }]
+              }]"
+            >
+              <a-select-option
+                v-for="account in accounts"
+                :key="account.id"
+                :value="account.name">
+                {{ account.name }}
+              </a-select-option>
+            </a-select>
+          </a-form-item>
+
+          <a-form-item :label="$t('project')" v-if="selectedScope === 'project'">
+            <a-select
+              v-decorator="['project', {
+                rules: [{ required: true, message: 'Required' }]
+              }]"
+            >
+              <a-select-option
+                v-for="project in projects"
+                :key="project.id"
+                :value="project.id">
+                {{ project.name }}
+              </a-select-option>
+            </a-select>
+          </a-form-item>
+        </a-form>
+      </a-spin>
+    </a-modal>
+
   </a-spin>
 </template>
 
 <script>
+import { api } from '@/api'
 
 export default {
   name: 'DedicatedVLANTab',
@@ -35,10 +138,21 @@ export default {
       default: false
     }
   },
+  inject: ['parentStartLoading', 'parentFinishLoading'],
   data () {
     return {
-      fetchLoading: false
+      fetchLoading: false,
+      formLoading: false,
+      items: [],
+      domains: [],
+      accounts: [],
+      projects: [],
+      modal: false,
+      selectedScope: 'account'
     }
+  },
+  beforeCreate () {
+    this.form = this.$form.createForm(this)
   },
   mounted () {
     this.fetchData()
@@ -52,12 +166,224 @@ export default {
   },
   methods: {
     fetchData () {
-      // List existing IP range etc.
+      this.form.resetFields()
+      api('listDedicatedGuestVlanRanges', {
+        physicalnetworkid: this.resource.id
+      }).then(response => {
+        this.items = response.listdedicatedguestvlanrangesresponse.dedicatedguestvlanrange
+          ? response.listdedicatedguestvlanrangesresponse.dedicatedguestvlanrange : []
+        this.parentFinishLoading()
+        this.formLoading = false
+      }).catch(error => {
+        this.$notification.error({
+          message: `Error ${error.response.status}`,
+          description: error.response.data.errorresponse.errortext
+        })
+        this.parentFinishLoading()
+        this.formLoading = false
+      })
+    },
+    fetchDomains () {
+      api('listDomains', {
+        details: 'min',
+        listAll: true
+      }).then(response => {
+        this.domains = response.listdomainsresponse.domain
+          ? response.listdomainsresponse.domain : []
+        if (this.domains.length > 0) {
+          this.form.setFieldsValue({
+            domain: this.domains[0].id
+          })
+          this.fetchAccounts(this.form.getFieldValue('domain'))
+        } else {
+          this.form.setFieldsValue({
+            domain: null
+          })
+        }
+        this.formLoading = false
+      }).catch(error => {
+        this.$notification.error({
+          message: `Error ${error.response.status}`,
+          description: error.response.data.errorresponse.errortext
+        })
+        this.formLoading = false
+      })
+    },
+    fetchAccounts (e) {
+      this.formLoading = true
+      api('listAccounts', {
+        domainid: e,
+        details: 'min',
+        listAll: true
+      }).then(response => {
+        this.accounts = response.listaccountsresponse.account
+          ? response.listaccountsresponse.account : []
+        if (this.accounts.length > 0) {
+          this.form.setFieldsValue({
+            account: this.accounts[0].name
+          })
+        } else {
+          this.form.setFieldsValue({
+            account: null
+          })
+        }
+        this.formLoading = false
+      }).catch(error => {
+        this.$notification.error({
+          message: `Error ${error.response.status}`,
+          description: error.response.data.errorresponse.errortext
+        })
+        this.formLoading = false
+      })
+    },
+    fetchProjects (e) {
+      this.formLoading = true
+      api('listProjects', {
+        domainid: e,
+        details: 'min'
+      }).then(response => {
+        this.projects = response.listprojectsresponse.project
+          ? response.listprojectsresponse.project : []
+        if (this.projects.length > 0) {
+          this.form.setFieldsValue({
+            project: this.projects[0].id
+          })
+        } else {
+          this.form.setFieldsValue({
+            project: null
+          })
+        }
+        this.formLoading = false
+      }).catch(error => {
+        this.$notification.error({
+          message: `Error ${error.response.status}`,
+          description: error.response.data.errorresponse.errortext
+        })
+        this.formLoading = false
+      })
+    },
+    handleDelete (item) {
+      this.parentStartLoading()
+      api('releaseDedicatedGuestVlanRange', {
+        id: item.id
+      }).then(response => {
+        this.$store.dispatch('AddAsyncJob', {
+          title: `Deleted ${item.id}`,
+          jobid: response.releasededicatedguestvlanrangeresponse.jobid,
+          status: 'progress'
+        })
+        this.$pollJob({
+          jobId: response.releasededicatedguestvlanrangeresponse.jobid,
+          successMethod: () => {
+            this.fetchData()
+            this.parentFinishLoading()
+          },
+          errorMessage: 'Deleting failed',
+          errorMethod: () => {
+            this.fetchData()
+            this.parentFinishLoading()
+          },
+          loadingMessage: `Deleting ${item.id}`,
+          catchMessage: 'Error encountered while fetching async job result',
+          catchMethod: () => {
+            this.fetchData()
+            this.parentFinishLoading()
+          }
+        })
+      }).catch(error => {
+        console.log(error)
+        this.$message.error('Failed to delete.')
+        this.fetchData()
+        this.parentFinishLoading()
+      })
+    },
+    handleSubmit (e) {
+      e.preventDefault()
+      this.form.validateFields(errors => {
+        if (errors) return
+
+        this.formLoading = true
+        this.parentStartLoading()
+        const fieldValues = this.form.getFieldsValue()
+
+        api('dedicateGuestVlanRange', {
+          physicalnetworkid: this.resource.id,
+          vlanrange: fieldValues.range,
+          domainid: fieldValues.domain,
+          account: fieldValues.account,
+          projectid: fieldValues.project
+        }).then(response => {
+          this.modal = false
+          this.fetchData()
+        }).catch(error => {
+          this.$notification.error({
+            message: `Error ${error.response.status}`,
+            description: error.response.data.dedicateguestvlanrangeresponse.errortext
+          })
+          this.modal = false
+          this.fetchData()
+        })
+      })
+    },
+    fetchBasedOnScope (e) {
+      if (e === 'account') this.fetchAccounts(this.form.getFieldValue('domain'))
+      if (e === 'project') this.fetchProjects(this.form.getFieldValue('domain'))
+    },
+    handleDomainChange () {
+      setTimeout(() => {
+        this.fetchBasedOnScope(this.selectedScope)
+      }, 100)
+    },
+    handleScopeChange (e) {
+      this.fetchBasedOnScope(e)
+    },
+    handleOpenModal () {
+      this.modal = true
+      this.formLoading = true
+      this.fetchDomains()
     }
   }
 }
 </script>
 
-<style lang="less" scoped>
+<style lang="scss" scoped>
+.list {
 
+  &__label {
+    font-weight: bold;
+  }
+
+  &__col {
+    flex: 1;
+
+    @media (min-width: 480px) {
+      &:not(:last-child) {
+        margin-right: 20px;
+      }
+    }
+
+  }
+
+  &__item {
+    margin-right: -8px;
+
+    &-outer-container {
+      width: 100%;
+    }
+
+    &-container {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+
+      @media (min-width: 480px) {
+        flex-direction: row;
+        margin-bottom: 10px;
+      }
+
+    }
+
+  }
+
+}
 </style>
