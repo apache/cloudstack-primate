6a3e4becc203b9c72f386cd3dec421ade52c5f6f
Rohit Yadav <rohit.yadav@shapeblue.com>
2020-03-26 02:20:15 +0530

    pg size fix
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- src/views/AutogenView.vue
+++ src/views/AutogenView.vue
@@ -235,7 +235,7 @@
         :pageSize="pageSize"
         :total="itemCount"
         :showTotal="total => `Total ${total} items`"
-        :pageSizeOptions="['20', '40', '80', '100', '500']"
+        :pageSizeOptions="['10', '20', '40', '80', '100', '500']"
         @change="changePage"
         @showSizeChange="changePageSize"
         showSizeChanger
@@ -297,7 +297,7 @@ export default {
       items: [],
       itemCount: 0,
       page: 1,
-      pageSize: 20,
+      pageSize: 10,
       searchQuery: '',
       resource: {},
       selectedRowKeys: [],
