55ea72f1ace5aebc51257ff9255c02229b7e6a23
Abhishek Kumar <abhishek.mrt22@gmail.com>
2020-05-06 14:23:37 +0530

    fix
    Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>

--- src/components/view/InfoCard.vue
+++ src/components/view/InfoCard.vue
@@ -545,7 +545,7 @@
               </a-button>
             </a-input-group>
           </div>
-          <a-tag @click="showInput" style="background: #fff; borderStyle: dashed;" v-if="inputVisible &&('createTag' in $store.getters.apis)">
+          <a-tag @click="showInput" style="background: #fff; borderStyle: dashed;" v-if="inputVisible &&('createTags' in $store.getters.apis)">
             <a-icon type="plus" /> New Tag
           </a-tag>
         </div>
