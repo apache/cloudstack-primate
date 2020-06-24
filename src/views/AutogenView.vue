5df7869cedccc6baf7e07cb7773db2d45142d041
Rohit Yadav <rohit.yadav@shapeblue.com>
2020-06-24 14:30:15 +0530

    Update AutogenView.vue

--- src/views/AutogenView.vue
+++ src/views/AutogenView.vue
@@ -740,8 +740,7 @@ export default {
       this.currentAction.paramFields.map(field => {
         let fieldValue = null
         let fieldName = null
-        if (field.type === 'list' || field.name === 'account' ||
-          (this.currentAction.mapping && field.name in this.currentAction.mapping && !(field.name in this.resource))) {
+        if (field.type === 'list' || field.name === 'account') {
           fieldName = field.name.replace('ids', 'name').replace('id', 'name')
         } else {
           fieldName = field.name
