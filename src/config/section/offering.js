07aa9529940c369f3da995e727d72bb420b20cd1
davidjumani <dj.davidjumani1994@gmail.com>
2020-07-10 13:53:27 +0530

    Adding resource type to vpcofferings

--- src/config/section/offering.js
+++ src/config/section/offering.js
@@ -252,8 +252,9 @@ export default {
       icon: 'deployment-unit',
       permission: ['listVPCOfferings', 'listInfrastructure'],
       params: { isrecursive: 'true' },
+      resourceType: 'VpcOffering',
       columns: ['name', 'state', 'displaytext', 'domain', 'zone', 'order'],
-      details: ['name', 'id', 'displaytext', 'distributedvpcrouter', 'service', 'domain', 'zone', 'created'],
+      details: ['name', 'id', 'displaytext', 'distributedvpcrouter', 'tags', 'service', 'domain', 'zone', 'created'],
       related: [{
         name: 'vpc',
         title: 'label.vpc',
