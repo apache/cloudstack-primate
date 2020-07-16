5ba2922ed084a15567fe1dfa199d330838d73829
Pearl Dsilva <pearl.dsilva@shapeblue.com>
2020-07-16 10:50:55 +0530

    Add contextual info to forms

--- src/views/infra/AddPrimaryStorage.vue
+++ src/views/infra/AddPrimaryStorage.vue
@@ -19,14 +19,26 @@
   <div class="form-layout">
     <a-spin :spinning="loading">
       <a-form :form="form" layout="vertical">
-        <a-form-item :label="$t('label.scope')">
+        <a-form-item>
+          <span slot="label">
+            {{ $t('label.scope') }}
+            <a-tooltip :title="apiParams.scope.description">
+              <a-icon type="info-circle" style="color: rgba(0,0,0,.45)" />
+            </a-tooltip>
+          </span>
           <a-select v-decorator="['scope', { initialValue: 'cluster' }]" @change="val => { this.scope = val }">
             <a-select-option :value="'cluster'"> {{ $t('label.clusterid') }} </a-select-option>
             <a-select-option :value="'zone'"> {{ $t('label.zoneid') }} </a-select-option>
           </a-select>
         </a-form-item>
         <div v-if="this.scope === 'zone'">
-          <a-form-item :label="$t('label.hypervisor')">
+          <a-form-item>
+            <span slot="label">
+              {{ $t('label.hypervisor') }}
+              <a-tooltip :title="apiParams.hypervisor.description">
+                <a-icon type="info-circle" style="color: rgba(0,0,0,.45)" />
+              </a-tooltip>
+            </span>
             <a-select
               v-decorator="['hypervisor', { initialValue: hypervisors[0]}]"
               @change="val => this.selectedHypervisor = val">
@@ -36,7 +48,13 @@
             </a-select>
           </a-form-item>
         </div>
-        <a-form-item :label="$t('label.zoneid')">
+        <a-form-item>
+          <span slot="label">
+            {{ $t('label.zoneid') }}
+            <a-tooltip :title="apiParams.zoneid.description">
+              <a-icon type="info-circle" style="color: rgba(0,0,0,.45)" />
+            </a-tooltip>
+          </span>
           <a-select
             v-decorator="['zone', { initialValue: this.zoneSelected, rules: [{ required: true, message: `${this.$t('label.required')}`}] }]"
             @change="val => changeZone(val)">
@@ -46,7 +64,13 @@
           </a-select>
         </a-form-item>
         <div v-if="this.scope === 'cluster' || this.scope === 'host'">
-          <a-form-item :label="$t('label.podid')">
+          <a-form-item>
+            <span slot="label">
+              {{ $t('label.podid') }}
+              <a-tooltip :title="apiParams.podid.description">
+                <a-icon type="info-circle" style="color: rgba(0,0,0,.45)" />
+              </a-tooltip>
+            </span>
             <a-select
               v-decorator="['pod', { initialValue: this.podSelected, rules: [{ required: true, message: `${this.$t('label.required')}`}] }]"
               @change="val => changePod(val)">
@@ -55,7 +79,13 @@
               </a-select-option>
             </a-select>
           </a-form-item>
-          <a-form-item :label="$t('label.clusterid')">
+          <a-form-item>
+            <span slot="label">
+              {{ $t('label.clusterid') }}
+              <a-tooltip :title="apiParams.clusterid.description">
+                <a-icon type="info-circle" style="color: rgba(0,0,0,.45)" />
+              </a-tooltip>
+            </span>
             <a-select
               v-decorator="['cluster', { initialValue: this.clusterSelected, rules: [{ required: true, message: `${this.$t('label.required')}`}] }]"
               @change="val => fetchHypervisor(val)">
@@ -76,7 +106,13 @@
             </a-select>
           </a-form-item>
         </div>
-        <a-form-item :label="$t('label.name')">
+        <a-form-item>
+          <span slot="label">
+            {{ $t('label.name') }}
+            <a-tooltip :title="apiParams.name.description">
+              <a-icon type="info-circle" style="color: rgba(0,0,0,.45)" />
+            </a-tooltip>
+          </span>
           <a-input v-decorator="['name', { rules: [{ required: true, message: `${this.$t('label.required')}` }] }]"/>
         </a-form-item>
         <a-form-item :label="$t('label.protocol')">
@@ -126,7 +162,13 @@
             <a-input v-decorator="['vCenterDataStore', { rules: [{ required: true, message: `${this.$t('label.required')}` }] }]"/>
           </a-form-item>
         </div>
-        <a-form-item :label="$t('label.providername')">
+        <a-form-item>
+          <span slot="label">
+            {{ $t('label.providername') }}
+            <a-tooltip :title="apiParams.provider.description">
+              <a-icon type="info-circle" style="color: rgba(0,0,0,.45)" />
+            </a-tooltip>
+          </span>
           <a-select
             v-decorator="['provider', { initialValue: providerSelected, rules: [{ required: true, message: `${this.$t('label.required')}`}] }]"
             @change="val => this.providerSelected = val">
@@ -136,18 +178,42 @@
           </a-select>
         </a-form-item>
         <div v-if="this.providerSelected !== 'DefaultPrimary'">
-          <a-form-item :label="$t('label.ismanaged')">
+          <a-form-item>
+            <span slot="label">
+              {{ $t('label.ismanaged') }}
+              <a-tooltip :title="apiParams.managed.description">
+                <a-icon type="info-circle" style="color: rgba(0,0,0,.45)" />
+              </a-tooltip>
+            </span>
             <a-checkbox-group v-decorator="['managed']" >
               <a-checkbox value="ismanaged"></a-checkbox>
             </a-checkbox-group>
           </a-form-item>
-          <a-form-item :label="$t('label.capacitybytes')">
+          <a-form-item>
+            <span slot="label">
+              {{ $t('label.capacitybytes') }}
+              <a-tooltip :title="apiParams.capacitybytes.description">
+                <a-icon type="info-circle" style="color: rgba(0,0,0,.45)" />
+              </a-tooltip>
+            </span>
             <a-input v-decorator="['capacityBytes']" />
           </a-form-item>
-          <a-form-item :label="$t('label.capacityiops')">
+          <a-form-item>
+            <span slot="label">
+              {{ $t('label.capacityiops') }}
+              <a-tooltip :title="apiParams.capacityiops.description">
+                <a-icon type="info-circle" style="color: rgba(0,0,0,.45)" />
+              </a-tooltip>
+            </span>
             <a-input v-decorator="['capacityIops']" />
           </a-form-item>
-          <a-form-item :label="$t('label.url')">
+          <a-form-item>
+            <span slot="label">
+              {{ $t('label.url') }}
+              <a-tooltip :title="apiParams.url.description">
+                <a-icon type="info-circle" style="color: rgba(0,0,0,.45)" />
+              </a-tooltip>
+            </span>
             <a-input v-decorator="['url']" />
           </a-form-item>
         </div>
@@ -174,7 +240,13 @@
             <a-input v-decorator="['volume']" />
           </a-form-item>
         </div>
-        <a-form-item :label="$t('label.storagetags')">
+        <a-form-item>
+          <span slot="label">
+            {{ $t('label.storagetags') }}
+            <a-tooltip :title="apiParams.tags.description">
+              <a-icon type="info-circle" style="color: rgba(0,0,0,.45)" />
+            </a-tooltip>
+          </span>
           <a-select
             mode="tags"
             v-model="selectedTags"
@@ -229,6 +301,11 @@ export default {
   },
   beforeCreate () {
     this.form = this.$form.createForm(this)
+    this.apiParams = {}
+    var apiConfig = this.$store.getters.apis.createStoragePool || {}
+    apiConfig.params.forEach(param => {
+      this.apiParams[param.name] = param
+    })
   },
   mounted () {
     this.fetchData()
