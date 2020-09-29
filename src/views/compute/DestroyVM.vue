dd152259b7d6e54cc87485cdbbc6432361beb1b3
utchoang <hoangnm@unitech.vn>
2020-09-29 15:34:13 +0700

    fix call fetchData after pollJob success response

--- src/views/compute/DestroyVM.vue
+++ src/views/compute/DestroyVM.vue
@@ -106,7 +106,6 @@ export default {
     },
     handleSubmit (e) {
       e.preventDefault()
-      const self = this
       this.form.validateFields((err, values) => {
         if (err) {
           return
@@ -137,11 +136,14 @@ export default {
             catchMessage: this.$t('error.fetching.async.job.result'),
             successMessage: `${this.$t('message.success.delete.vm')} ${this.resource.name}`,
             successMethod: () => {
-              if (self.$route.path.includes('/vm/') && values.expunge) {
-                self.$router.go(-1)
+              if (this.$route.path.includes('/vm/') && values.expunge) {
+                this.$router.go(-1)
               } else {
-                self.parentFetchData()
+                this.parentFetchData()
               }
+            },
+            action: {
+              api: 'destroyVirtualMachine'
             }
           })
           this.closeAction()
