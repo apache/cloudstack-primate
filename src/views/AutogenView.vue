207dbe06a5a6726e72d607d7b49b4784f87633ee
utchoang <hoangnm@unitech.vn>
2020-12-21 13:05:26 +0700

    add missing translation labels

--- src/views/AutogenView.vue
+++ src/views/AutogenView.vue
@@ -123,6 +123,8 @@
         :visible="showAction"
         :closable="true"
         :maskClosable="false"
+        :okText="$t('label.ok')"
+        :cancelText="$t('label.cancel')"
         style="top: 20px;"
         @ok="handleSubmit"
         @cancel="closeAction"
